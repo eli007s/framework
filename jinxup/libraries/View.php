5ddc73bd111a42bcb37d90400b37f32decca2bdb
Eliezer Orellana <eli007s@gmail.com>
2015-09-14 09:07:18 -0700

    dev view

--- jinxup/libraries/View.php
+++ jinxup/libraries/View.php
@@ -14,30 +14,39 @@
 
 		public static function __callStatic($name, $params)
 		{
-			self::_viewInit();
-
+			$config = JXP_Config::get('template');
 			$return = null;
 
-			try
+			if (isset($config['use']))
 			{
-				if (count($params) == 0)
-					$return = self::$_smarty->{$name}();
+				$use = $config['use'];
 
-				if (count($params) == 1)
-					$return = self::$_smarty->{$name}($params[0]);
+				if ($use == 'smarty')
+				{
+					self::_viewInit([], $use);
 
-				if (count($params) == 2)
-					$return = self::$_smarty->{$name}($params[0], $params[1]);
+					try
+					{
+						if (count($params) == 0)
+							$return = self::$_smarty->{$name}();
 
-				if (count($params) == 3)
-					$return = self::$_smarty->{$name}($params[0], $params[1], $params[2]);
+						if (count($params) == 1)
+							$return = self::$_smarty->{$name}($params[0]);
 
-				if (count($params) == 4)
-					$return = self::$_smarty->{$name}($params[0], $params[1], $params[2], $params[3]);
+						if (count($params) == 2)
+							$return = self::$_smarty->{$name}($params[0], $params[1]);
 
-			} catch (Exception $e) {
+						if (count($params) == 3)
+							$return = self::$_smarty->{$name}($params[0], $params[1], $params[2]);
 
-				$return = $e->getMessage();
+						if (count($params) == 4)
+							$return = self::$_smarty->{$name}($params[0], $params[1], $params[2], $params[3]);
+
+					} catch (Exception $e) {
+
+						$return = $e->getMessage();
+					}
+				}
 			}
 
 			return $return;
@@ -56,7 +65,11 @@
 			self::$_config = $config;
 		}
 
+<<<<<<< Updated upstream
 		private static function _viewInit($_vars = array())
+=======
+		private static function _viewInit($_vars = [], $engine = null)
+>>>>>>> Stashed changes
 		{
 			if (is_null(self::$_smarty))
 			{
@@ -86,6 +99,7 @@
 					'param'      => JXP_Routes::getParams()
 				);
 
+<<<<<<< Updated upstream
 				$_vars['app'] = array_merge($_vars['app'], $vars);
 
 				$vars = array();
@@ -109,17 +123,57 @@
 				self::$_smarty->addPluginsDir(self::$_paths['views'] . DS . 'plugins');
 				self::$_smarty->registerResource('file', new RecompileFileResource());
 				self::$_smarty->muteExpectedErrors();
+=======
+			if ($engine == 'smarty')
+			{
+				if (is_null(self::$_smarty))
+				{
+					$smartyPath = dirname(__DIR__) . DS . 'engines' . DS . 'smarty' . DS . 'Smarty.class.php';
+
+					if (file_exists($smartyPath))
+					{
+						require_once($smartyPath);
+
+						self::$_smarty                  = new Smarty();
+						self::$_params['app']           = self::getApp();
+						self::$_smarty->left_delimiter  = '{!';
+						self::$_smarty->right_delimiter = '!}';
+
+						if (isset(self::$_config['template']['delimiters']['left']))
+							self::$_smarty->left_delimiter = self::$_config['template']['delimiters']['left'];
+
+						if (isset(self::$_config['template']['delimiters']['right']))
+							self::$_smarty->right_delimiter = self::$_config['template']['delimiters']['right'];
+
+						self::$_smarty->assign('app', $_vars['app']);
+						self::$_smarty->assign('jxp', $_vars['jxp']);
+						self::$_smarty->setTemplateDir(self::$_paths['views']);
+						self::$_smarty->addPluginsDir(self::$_paths['views'] . DS . 'plugins');
+						self::$_smarty->registerResource('file', new RecompileFileResource());
+						self::$_smarty->muteExpectedErrors();
+					}
+				}
+>>>>>>> Stashed changes
 			}
 		}
 
 		public static function set($key, $val)
 		{
+<<<<<<< Updated upstream
 			self::_viewInit();
 
 			self::$_smarty->assign($key, $val);
+=======
+			self::$_vars[$key] = $val;
 		}
 
-		public static function render($tpl)
+		public static function get($key)
+		{
+			return isset(self::$_vars[$key]) ? self::$_vars[$key] : null;
+>>>>>>> Stashed changes
+		}
+
+		public static function render($tpl, $options = array())
 		{
 			$_vars = array();
 
@@ -150,17 +204,68 @@
 				}
 			}
 
+<<<<<<< Updated upstream
 			self::viewInit($_vars);
 
 			try
 			{
 				self::$_smarty->display(self::$_paths['views'] . DS . ltrim($tpl, '/'));
+=======
+			if (substr($tpl, -4) == '.php')
+			{
+				self::_viewInit($_vars);
+>>>>>>> Stashed changes
 
 			} catch (SmartyException $e) {
 
+<<<<<<< Updated upstream
 				$error = $e->getMessage();
 
 				if (preg_match('/Syntax error in template/im', $error, $match))
+=======
+				if (is_array($options) && !empty($options))
+				{
+					foreach ($options as $key => $val)
+					{
+						foreach ($val as $k => $v)
+							$$k = $v;
+					}
+				}
+
+				self::$_vars = [];
+
+				require_once(self::$_paths['views'] . DS . ltrim($tpl, '/'));
+
+			} else {
+
+				$config = JXP_Config::get('template');
+				$use    = 'smarty';
+
+				if (isset($config['use']))
+				{
+					$use = $config['use'];
+				}
+
+				if (is_array($options))
+				{
+					if (is_array($options) && !empty($options))
+					{
+						foreach ($options as $key => $val)
+						{
+							foreach ($val as $k => $v)
+								self::$_vars[$k] = $v;
+						}
+					}
+
+					self::_viewInit([], $use);
+
+				} else {
+
+
+				}
+
+				try
+>>>>>>> Stashed changes
 				{
 					self::set('error', $e->getMessage());
 					self::_logExit('template_syntax', __LINE__);
